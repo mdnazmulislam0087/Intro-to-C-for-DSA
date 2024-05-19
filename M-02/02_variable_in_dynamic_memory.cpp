1e5dc55fa1da87d9d4548549b8f472893a8c34ed
MD NAZMUL ISLAM <md.nazmul.islam0087@gmail.com>
2024-05-20 00:56:57 +0600

    02_variable_in_dynamic_memory

---
+++ M-02/02_variable_in_dynamic_memory.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int *p = new int;
+    *p = 400;
+    cout << "Value of P and address from Heap " <<p <<endl;
+    cout << *p;
+
+    return 0;
+}
