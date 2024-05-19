1920b5a2bc258b2f540c695909b3859180dee76d
MD NAZMUL ISLAM <md.nazmul.islam0087@gmail.com>
2024-05-20 00:44:01 +0600

    first commit

---
+++ M-01/output.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+
+int main()
+{
+    // std::cout <<"Hello World";
+    int a = 100;
+    long long int b = 100000000000;
+    float c = 1.5;
+    double d = 9999.222;
+    char e = 'A';
+
+    // std:: cout << a;
+    // std::cout <<"My favourite no is "<< a;
+    // std::cout <<a <<" is my favourite numer";
+    // std:: cout <<a <<" "<< b;
+    // std::cout <<a<<"\n"<<b;
+    std::cout <<a<<std::endl<<b;
+
+    return 0;
+}
